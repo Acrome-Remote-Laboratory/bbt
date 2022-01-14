9dc44358b9f49e1501a745acaae3b2328b4294e3
candancaner <candancaner@gmail.com>
2022-01-14 23:08:10 +0300

    Update bbt_example.py
    code refactored,

--- src/bbt_example.py
+++ src/bbt_example.py
@@ -10,14 +10,14 @@ interval = 0.004 # Control period in seconds
 error_sum_x = 0
 error_sum_y = 0
 _filter_size = 10
-bbt_iter = 0
+#bbt_iter = 0
 x_mov_avg_filter = [0 for i in range(_filter_size)]
 y_mov_avg_filter = [0 for i in range(_filter_size)]
 
 #Control Parameters
 feedforwardx = 725
 feedforwardy = 725
-windup_abs = 10
+windup_abs = 30
 calibration_x = (0,-50)
 calibration_y = (0,-8)
 
@@ -42,37 +42,39 @@ while True:
     positionx = 1000 if positionx > 1000 else positionx
     positiony = 1000 if positiony > 1000 else positiony
 
-    positionx += -100
-    positiony += 0
+    #positionx += -100
+    #positiony += 0
 
-
-    setpointx = (setpointx + 50) * 3 + 250
-    setpointy = (setpointy + 60) * 3 + 250
+    #setpointx = (setpointx + 50) * 3 + 250
+    #setpointy = (setpointy + 60) * 3 + 250
 
     error = ((setpointx - positionx), (setpointy - positiony))
     error_sum_x += error[0]
     error_sum_y += error[1]
 
     #PID for X axis
-    px = error[0] * kpx
-    ix = error_sum_x*kix
-    ix = (ix if -windup_abs <= ix <= windup_abs else ix/abs(ix)*windup_abs)
+    px =  error_sum_x * kpx
+    ix = error_sum_x * kix
+    ix = ix if -windup_abs <= ix <= windup_abs else (ix/abs(ix))*windup_abs
 
     x_mov_avg_filter[bbt_iter] = (error[0] - err_prev[0]) * kdx / interval
     dx = sum(x_mov_avg_filter[0:_filter_size])/_filter_size
 
     #PID for Y axis
-    py = error[1] * kpy
-    iy = error_sum_y*kiy
-    iy = (iy if -windup_abs <= iy <= windup_abs else iy/abs(iy)*windup_abs)
+    py =  error_sum_y * kpy
+    iy = error_sum_y * kiy
+    iy = iy if -windup_abs <= iy <= windup_abs else (iy/abs(iy))*windup_abs
 
     y_mov_avg_filter[bbt_iter] = (error[1] - err_prev[1]) * kdy / interval
     dy = sum(y_mov_avg_filter[0:_filter_size])/_filter_size
 
-    bbt_iter = (bbt_iter + 1) % _filter_size
-
-    outputx = feedforwardx + px + ix + dx
-    outputy = feedforwardy + py + iy + dy
+    #bbt_iter = (bbt_iter + 1) % _filter_size
+    
+    if abs(error_sum_x)>10:
+        outputx = feedforwardx + px + ix + dx
+    
+    if abs(error_sum_y)>10:
+        outputy = feedforwardy + py + iy + dy
 
     outputx += calibration_x[0] * positionx + calibration_x[1]
     outputy += calibration_y[0] * positiony + calibration_y[1]
@@ -86,9 +88,9 @@ while True:
     outputy = 1000 if outputy >= 1000 else outputy
 
     ####
-    positionx = (positionx - 250) / 3 - 50
-    positiony = (positiony - 250) / 3 - 60
+    #positionx = (positionx - 250) / 3 - 50
+    #positiony = (positiony - 250) / 3 - 60
     bbt.set_servo(int(outputx), int(outputy))
     
-    err_prev = error
+    #err_prev = error
     time.sleep(interval)
